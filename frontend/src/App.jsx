a6921e206f23ac2c46fe2eae620b604e333be9c8
Jagannath <jagannathdebgunin@gmail.com>
2025-05-31 18:37:32 +0530

    Setting up frontend before tanstack query

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
+import { Route,Routes } from 'react-router';
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import NotificationsPage from './pages/NotificationsPage.jsx';
+import LoginPage from "./pages/LoginPage.jsx";
+import ChatPage from "./pages/ChatPage.jsx"
+import CallPage from "./pages/CallPage.jsx"
+import OnboardingPage from "./pages/OnboardingPage.jsx"
+import { Toaster } from 'react-hot-toast';
+const  App= ()=> {
+ return <div className="h-screen" data-theme="night">
 
-function App() {
- return <div className="bg-red-500 h-screen text-5xl"><button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Primary</button>
-<button className="btn btn-secondary">Secondary</button>
-<button className="btn btn-accent">Accent</button>
-<button className="btn btn-info">Info</button>
-<button className="btn btn-success">Success</button>
-<button className="btn btn-warning">Warning</button>
-<button className="btn btn-error">Error</button></div>
-}
+    
+    <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+        <Route path="/call" element={<CallPage/>} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
+    </Routes>
+
+    <Toaster/>
+
+
+ </div>
+};
 
 export default App
---
+++ frontend/src/pages/NotificationsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotificationsPage = () => {
+  return (
+    <div>
+      
+      Notifications Page
+    </div>
+  )
+}
+
+export default NotificationsPage
