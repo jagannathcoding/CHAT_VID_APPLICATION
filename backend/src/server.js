a8cab7dd1c5175d595899512a5163c225d840ede
Jagannath <jagannathdebgunin@gmail.com>
2025-06-07 23:59:08 +0530

    now start signup page

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,8 +5,19 @@ import {connectDB} from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js"
+import cors from "cors";
+
+
+
 const app=express();
 const PORT=process.env.PORT;
+
+app.use(
+    cors({
+    origin:"http://localhost:5173",
+    credentials:true//alow frontend to send cookies
+})
+);
 
 
 
