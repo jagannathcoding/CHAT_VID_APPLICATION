bc78fdb2bdc27fe9dad1ed81d50f5b2d226778e9
Jagannath <jagannathdebgunin@gmail.com>
2025-06-17 20:53:13 +0530

    Added navbar sidebar and logout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import OnboardingPage from "./pages/OnboardingPage.jsx"
 import { Toaster } from 'react-hot-toast';
 import PageLoader from './components/PageLoader.jsx';
 import useAuthUser from './hooks/useAuthUser.js';
-
+import Layout from './components/Layout.jsx';
 const App = () => {
 
     const { isLoading, authUser } = useAuthUser();
@@ -31,7 +31,9 @@ const App = () => {
                 <Route path="/"
                     element={
                         isAuthenticated && isOnboarded ? (
-                            <HomePage />
+                           <Layout showSidebar={true}>
+                             <HomePage />
+                           </Layout>
                         ) : (
                             <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
                         )
--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -11,12 +11,23 @@ export const login=async(loginData)=>{
     return response.data;
 }
 
+export const logout=async()=>{
+    const response=await axiosInstance.post("/auth/logout");
+    return response.data;
+}
+
 
 
 
 export const getAuthUser = async () => {
-    const res = await axiosInstance.get("/auth/me");
+    try{const res = await axiosInstance.get("/auth/me");
     return res.data;
+    }
+    catch(error)
+    {
+        console.log("Error in getAuthUser:",error);
+        return null;
+    }
 };
 
 
