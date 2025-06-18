9b1de32ce2ad2936375145cb274708b660ea407c
Jagannath <jagannathdebgunin@gmail.com>
2025-06-18 13:32:10 +0530

    Doing home state and added some codes in api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -40,3 +40,24 @@ export const completeOnboarding = async (userData) => {
 
 }
 
+
+export async function getUserFriends(){
+    const response=await axiosInstance.get("users/friends");
+    return response.data;
+}
+
+export async function getRecommendedUsers(){
+    const response=await axiosInstance.get("users");
+    return response.data;
+}
+
+
+export async function getOutgoingFriendReqs(){
+    const response=await axiosInstance.get("users/outgoing-friend-requests");
+    return response.data;
+}
+
+export async function sendFriendRequest(userId){
+    const response=await axiosInstance.post(`users/friend-request/${userId}`);
+    return response.data;
+}
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useThemeStore } from '../store/useThemeStore'
-
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import { getOutgoingFriendReqs, getRecommendedUsers, getUserFriends, sendFriendRequest } from '../lib/api'
 const HomePage = () => {
-  
 
-  
-  
+  const QueryClient = useQueryClient();
+  const [outgoingRequestIds, setOutgoingRequestIds] = useState(new Set())
+
+  const { data: friends = [], isLoading: loadingFriends } = useQuery({
+    queryKey: ["friends"],
+    queryFn: getUserFriends,
+  })
+
+
+  const { data: recommendedUsers = [], isLoading: loadingUsers } = useQuery({
+    queryKey: ["users"],
+    queryFn: getRecommendedUsers,
+  })
+
+  const { data: outgoingFriendReqs } = useQuery({
+    queryKey: ["outgoingFriendReqs"],
+    queryFn: getOutgoingFriendReqs,
+  });
+
+  const {mutate:sendRequestMutation,isPending}=useMutation({
+    mutationFn:sendFriendRequest,
+    onSuccess:()=>QueryClient.invalidateQueries({queryKey:["outgoingFriendReqs"]})
+  })
+
+  useEffect(()=>{
+    const outgoingIds=new Set()
+    if(outgoingFriendReqs&&outgoingFriendReqs.length>0){
+      outgoingFriendReqs.forEach((req)=>{
+        outgoingIds.add(req.id)
+      })
+    }
+  },[outgoingFriendReqs])
+
   return (
     <div>
       Home Page
